2c7ee4ca13d830fd4aeec7af8daf95c2a1ae6d7c
dmcomer <dfaruk106@gmail.com>
2026-08-28 21:17:44 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,5 +14,13 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
