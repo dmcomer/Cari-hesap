16e078fa793c2a738d063b85b92e97cf69b0c660
dmcomer <dfaruk106@gmail.com>
2026-08-28 20:23:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cari-hesap-v1';
+const CACHE_NAME = 'cari-hesap-v2';
 const FILES_TO_CACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
